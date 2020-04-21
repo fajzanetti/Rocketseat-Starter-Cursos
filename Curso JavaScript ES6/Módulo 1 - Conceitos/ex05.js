19ea99dc1f817e963d66050e412adb16fd706dcb
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-20 23:59:26 -0300

    Curso JS ES6 - Rest-Spread

--- Curso JavaScript ES6/Módulo 1 - Conceitos/ex05.js
+++ Curso JavaScript ES6/Módulo 1 - Conceitos/ex05.js
@@ -1,5 +1,6 @@
 const arr = [1, 2, 3, 4, 5, 6]
 
+// 5.1 Rest
 const [x, ...y] = arr
 console.log(x); // 1
 console.log(y); // [2, 3, 4, 5, 6]
@@ -8,6 +9,7 @@ const soma = (...params) => params.reduce((total, prox) => total + prox)
 console.log(soma(1, 2, 3, 4, 5, 6)); // 21
 console.log(soma(1, 2)); // 3
 
+// 5.2 Spread
 const usuario = {
     nome: 'Diego',
     idade: 23,
