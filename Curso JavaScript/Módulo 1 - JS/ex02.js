96896c59927e6e7746cb3398ed7a568e9eb8d287
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-21 00:10:43 -0300

    Curso JS - Function-For-If Simple

--- Curso JavaScript/Módulo 1 - JS/ex02.js
+++ Curso JavaScript/Módulo 1 - JS/ex02.js
@@ -1,7 +1,4 @@
-// Crie uma função que dado um intervalo (entre x e y) exiba todos número pares:
-
 function pares(x, y) {
-    // código aqui
     for (i=x; i<=y; i++){
         if (i % 2 == 0){
             console.log("Par: "+i)
