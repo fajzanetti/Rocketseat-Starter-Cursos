604c1c75cc4c2de03b892e9f5b8c6327bdfba126
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-21 00:01:12 -0300

    Curso JS ES6 - Object Short Syntax

--- Curso JavaScript ES6/Módulo 1 - Conceitos/ex07.js
+++ Curso JavaScript ES6/Módulo 1 - Conceitos/ex07.js
@@ -1,5 +1,12 @@
 const nome = 'Diego';
 const idade = 23;
+
+// const usuario = {
+//     nome: nome,
+//     idade: idade,
+//     cidade: 'Rio do Sul',
+// };
+
 const usuario = {
     nome,
     idade,
