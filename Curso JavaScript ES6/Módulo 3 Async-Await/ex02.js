4e189850ebcb2bbad572343633f58c6fd7dc49eb
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-20 23:44:37 -0300

    Curso JS ES6 - Function Axios-Async-Await

--- Curso JavaScript ES6/Módulo 3 Async-Await/ex02.js
+++ Curso JavaScript ES6/Módulo 3 Async-Await/ex02.js
@@ -2,12 +2,11 @@ const axios = require('axios')
 
 async function getUserFromGithub(user) {
     try {
-        const response = await axios.get(`https://api.github.com/users/${user}`)
-        console.log(response.data);
+        const res = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(res.data);
     } catch (error){
         console.log('Usuário não existe');
     }
 }
 getUserFromGithub('diego3g');
 getUserFromGithub('diego3g124123');
-getUserFromGithub('fajzanetti');
