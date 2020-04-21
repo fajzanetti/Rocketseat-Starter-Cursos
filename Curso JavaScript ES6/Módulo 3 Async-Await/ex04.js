a29a7020f18196d18e184f3b8cbbaf189606f87c
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-20 23:46:47 -0300

    Curso JS ES6 - Arrow Function Axios-Async-Await

--- Curso JavaScript ES6/Módulo 3 Async-Await/ex04.js
+++ Curso JavaScript ES6/Módulo 3 Async-Await/ex04.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 
 const buscaUsuario = async usuario => {
     try{
-        const res = await axios.get(`https://api.github.com/users/${usuario}`)
-        console.log(res.data)
+        const response = await axios.get(`https://api.github.com/users/${usuario}`)
+        console.log(response.data)
     } catch{
         console.log('Usuário não existe')
     }
