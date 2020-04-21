16039fc7e981f09c379d975728d1bea4c88ca616
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-21 00:12:55 -0300

    Curso JS - Function-If Simple

--- Curso JavaScript/Módulo 1 - JS/ex04.js
+++ Curso JavaScript/Módulo 1 - JS/ex04.js
@@ -1,10 +1,4 @@
-// Escreva uma função que dado um total de anos de estudo retorna o quão experiente o usuário é:
-// De 0-1 ano: Iniciante
-// De 1-3 anos: Intermediário
-// De 3-6 anos: Avançado
-// De 7 acima: Jedi Master
 function experiencia(anos) {
-    // código aqui
     if (anos >= 0 && anos < 1)
         console.log("Iniciante")
     else if (anos >= 1 && anos < 3)
