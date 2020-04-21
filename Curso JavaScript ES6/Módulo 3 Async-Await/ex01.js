a9c58961cc67a1f575f9fe448d2642bfb3c479a9
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-20 23:43:51 -0300

    Curso JS ES6 - Promise Async-Await

--- Curso JavaScript ES6/Módulo 3 Async-Await/ex01.js
+++ Curso JavaScript ES6/Módulo 3 Async-Await/ex01.js
@@ -3,10 +3,10 @@ const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 
 async function umPorSegundo() {
     await delay()
-    console.log('1 segundo');
+    console.log('1s');
     await delay()
-    console.log('2 segundos');
+    console.log('2s');
     await delay()
-    console.log('3 segundos');
+    console.log('3s');
 }
 umPorSegundo();
