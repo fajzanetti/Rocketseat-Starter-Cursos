ba630428b5f9b02c3394aa648eadfab6c9b15156
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-22 12:21:39 -0300

    CRUD

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 
+// Iniciando o App
 const app = express()
+app.use(express.json())
 
+// Iniciando o DB
 mongoose.connect('mongodb+srv://felipe:zanetti@cluster0-olkok.mongodb.net/starterNodeJS?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -11,6 +14,7 @@ mongoose.connect('mongodb+srv://felipe:zanetti@cluster0-olkok.mongodb.net/starte
 
 requireDir('./src/models')
 
+// Rotas
 app.use('/api', require('./src/routes'))
 
 app.listen(3001)
