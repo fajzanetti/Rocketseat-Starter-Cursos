7ad8a04ced555de087bafa7c7737ec0228bc7bf7
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-23 08:54:18 -0300

    CORS

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -3,10 +3,12 @@ require("dotenv").config()
 const express = require('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 // Iniciando o App
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // Iniciando o DB
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-olkok.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
