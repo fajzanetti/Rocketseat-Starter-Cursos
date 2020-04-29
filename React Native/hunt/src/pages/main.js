aebdc7fb24ee0eec597f546ee4257f80ff7e015d
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-04-29 04:30:49 -0300

    Buscando da API

--- React Native/hunt/src/pages/main.js
+++ React Native/hunt/src/pages/main.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import api from '../services/api'
+
 export default class main extends Component {
+
+    componentDidMount(){
+        this.loadProducts()
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products')
+
+        const { docs } = response.data
+
+        console.log(docs)
+    }
+
     render() {
         return (
             <View>
